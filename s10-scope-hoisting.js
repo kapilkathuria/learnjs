80b7106f0f7e86292b95bee5f67815b2dd5435c8
Kapil Kathuria <kapil.kathuria@gmail.com>
2020-04-12 10:05:12 +0530

    section 10 and course completed

---
+++ s10-scope-hoisting.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Scoope and hoisting
+// global scope
+// function scope
+// var and 
+// undecalred variables
+// strict mode
+
+// hoisting: function decaratin is hoisted. in first pass, all functions are
+// complied and in 2nd rest of the code
+
+// pollluting the global scope
+
+// best practices
+// define single const object in global scope
+//  and define all the variables in this object
