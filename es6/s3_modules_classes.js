7b7498c211219b2040866407ce2de0b2c6962f6e
Kapil Kathuria <kapil.kathuria@gmail.com>
2020-04-18 06:51:07 +0530

    Section 3 in Progress - Classes in Progress

--- es6/s3_modules_classes.js
+++ es6/s3_modules_classes.js
@@ -22,5 +22,36 @@ console.log(abc);
 // Assigning Alias
 // See import {keyValue as key above
 
-// to imported everything
+// to import everything, import * as imported from "./external.js"
+// can be used
+// imported is returned as array. each variable can be 
+// used as imported.varName
+
+// --------------------CLASSES-----------------------------
+// Class is similar to features, ussed in OOPS
+class Person {
+    // TO add properties to Person calss
+    constructor(name) {
+        this.name = name;
+    }
+    // greet is method here, not function
+    greet() {
+        console.log("hello " + this.name);
+    }
+}
+
+let person = new Person("Kapil");
+console.log(person);
+person.greet();
+
+// Class and Prototype
+// So, any method written inside class is prototype function
+// of that class
+// TO understand what is prototype 
+// See https://www.youtube.com/watch?v=hS_WqkyUah8
+// In Summary, prototype ensure, function code is not repeated 
+// in Memory
+console.log(person.__proto__ == Person.prototype)
+
+// Inheritence
 
